8c27fbdb4e160ff18d65b911b5759b7be30e3c83
justinbrick <justinbrick1@gmail.com>
2021-09-13 01:30:30 -0400

    Made games in a struct, added coin flip game, found out how lifetimes work.

--- src/game_manager.rs
+++ src/game_manager.rs
@@ -1,19 +1,62 @@
 use std::io;
+use std::io::Write;
 use std::num::ParseIntError;
 use rand::Rng;
 
+type GameFunction = fn() -> bool;
+
 pub struct Game {
-    pub play : fn(&self) -> bool,
-    pub name : String
+    pub play : GameFunction,
+    pub name : &'static str 
+}
+
+
+const COIN_FLIP_MAX : i16 = 300;
+fn coin_flip_game() -> bool {
+    let mut currency : i16 = 120;
+    while currency < COIN_FLIP_MAX {
+        let mut answer = String::new();
+        let random_choice : u8 = rand::thread_rng().gen_range(0..=1);
+        println!("Your current balance ({})", currency);
+        print!("Bet Amount: ");
+        io::stdout().flush().expect("Could not flush stream.");
+        io::stdin().read_line(&mut answer).expect("Could not read answer");
+        let bet_amount : Result<u8, ParseIntError> = answer.trim().parse();
+        let bet_amount : u8 = match bet_amount {
+            Ok(bet) => bet,
+            Err(e) => {
+                println!("{}", e);
+                1
+            }
+        };
+
+        print!("Heads or tails? (h/t): ");
+        io::stdout().flush().expect("Could not flush stream.");
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("Could not read answer");
+        let user_choice : u8 = answer.to_lowercase().starts_with("h") as u8;
+        if user_choice == random_choice {
+            currency += bet_amount as i16;
+        } else {
+            currency -= bet_amount as i16;
+        }
+        if currency < 1 {
+            return false;
+        } else if currency > COIN_FLIP_MAX {
+            return true;
+        }
+    }
+    return true;
 }
 
 fn guessing_game() -> bool {
     let mut attempts : i8 = 3;
     let random_number = rand::thread_rng().gen_range(1..=10);
-    println!("{}", random_number);
     while attempts > 0 {
         let mut answer = String::new();
-        println!("Enter a number from 1-10!");
+        print!("Enter a number from 1-10! ");
+        io::stdout().flush().expect("Could not flush the output stream!");
         io::stdin()
             .read_line(&mut answer)
             .expect("Could not read line!");
@@ -43,10 +86,13 @@ fn guessing_game() -> bool {
     return false;
 }
 
-const game_list : [Game; 1] = [
-    Game{name:String::from("Test"),play:guessing_game}
+const GAME_LIST_SIZE : usize = 2;
+const GAME_LIST : [Game; GAME_LIST_SIZE] = [
+    Game{name:"Number Guessing",play:guessing_game},
+    Game{name:"Coinflip", play:coin_flip_game}
 ];
 
-pub fn random() -> Game {
-    return game_list[rand::thread_rng().gen_range(0..game_list.len())]
+// What the fuck???
+pub fn random<'a>() -> &'a Game {
+    return &GAME_LIST[rand::thread_rng().gen_range(0..GAME_LIST_SIZE)];
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
+use std::io;
+use std::io::Write;
 mod game_manager;
 const GAME_VERSION : &str = "0.1.0";
 
 fn main() {
-    println!("Example Game {}", GAME_VERSION);
-    let game = game_manager::random();
-    println!("Chose random game {}...", game.name);
-    if game.play() {
-        println!("Congrats, loser!");
+    let mut points : u16 = 0;
+    let mut exit : bool = false;
+    println!("Rust Tests, Version {}", GAME_VERSION);
+    while !exit {
+        let game = game_manager::random();
+        let mut answer = String::new();
+        println!("Chose random game {}...", game.name);
+        if (game.play)() {
+            points += 10;
+            println!("Congrats! +10 Points!");
+            println!("You now have {} points.", points);
+        }
+        
+        print!("Would you like to exit? (Y/N) ");
+        io::stdout().flush().expect("Was unable to flush the output stream."); // You have to flush the stream for some reason! Yay :)
+        while answer.is_empty() {
+            io::stdin()
+            .read_line(&mut answer)
+            .expect("Unable to read string...");
+        }
+        
+        exit = answer.to_lowercase().starts_with("y");
     }
 }
