be499a4d2005184026d84aa00cd3ddd20acd7fa1
justinbrick <justinbrick1@gmail.com>
2021-09-13 12:01:25 -0400

    Added hang man game

--- src/game_manager.rs
+++ src/game_manager.rs
@@ -1,10 +1,12 @@
 use std::io;
 use std::io::Write;
 use std::num::ParseIntError;
+use std::char::ParseCharError;
 use rand::Rng;
 
 type GameFunction = fn() -> bool;
 
+// These will be compile time constants, I'm pretty sure.
 pub struct Game {
     pub play : GameFunction,
     pub name : &'static str 
@@ -86,10 +88,66 @@ fn guessing_game() -> bool {
     return false;
 }
 
-const GAME_LIST_SIZE : usize = 2;
+const WORD_LIST_SIZE : usize = 9;
+const WORD_LIST : [&str; WORD_LIST_SIZE] = [
+    "Rust", "Programming", "Video",
+    "Half-Life", "Sandwich", "Functional",
+    "Minecraft", "Tetris", "Mario"
+];
+
+fn hangman_game() -> bool {
+    let mut attempts : i8 = 10;
+    println!("Starting a new game of hangman! You have 10 attempts to guess the word. Please guess each character individually.");
+    let random_word = WORD_LIST[rand::thread_rng().gen_range(0..WORD_LIST_SIZE)];
+    let random_word : Vec<char> = random_word.chars().collect(); // Turn this into a char array.
+    let mut blanked : Vec<char> = random_word.clone(); 
+    for i in 0..blanked.len() {
+        let character = blanked[i];
+        if character != ' ' {
+            blanked[i] = '_';
+        }
+    }
+    while attempts != 0 {
+        let mut tmp = [0u8;4];
+        let mut blanked_string = String::new();
+        for character in &blanked {blanked_string += character.encode_utf8(&mut tmp);}
+        println!("{}", blanked_string);
+        print!("Guess a letter: ");
+        io::stdout().flush().expect("Could not flush stream.");
+        let mut answer = String::new();
+        io::stdin().read_line(&mut answer).expect("Could not read input.");
+        let answer : Result<char, ParseCharError> = answer.trim().parse();
+        let answer = match answer {
+            Ok(result) => result,
+            Err(e) => {
+                println!("{}", e);
+                '-'
+            }
+        };
+        let mut found = false;
+        for i in 0..random_word.len() {
+            let character = random_word[i].to_ascii_lowercase();
+            if answer == character && blanked[i] == '_' {
+                blanked[i] = random_word[i];
+                found = true;
+            }
+        }
+        if !found { 
+            attempts -= 1;
+            println!("{} is not in this word! -1 attempt. You now have {} attempts left.", answer, attempts);
+        } else if random_word == blanked {
+            println!("You have beat the game! Congratulations.");
+            return true;
+        }
+    }
+    return false;
+}
+
+const GAME_LIST_SIZE : usize = 3;
 const GAME_LIST : [Game; GAME_LIST_SIZE] = [
-    Game{name:"Number Guessing",play:guessing_game},
-    Game{name:"Coinflip", play:coin_flip_game}
+    Game{name:"Number Guessing", play:guessing_game},
+    Game{name:"Coinflip", play:coin_flip_game},
+    Game{name:"Hangman", play:hangman_game}
 ];
 
 // What the fuck???
